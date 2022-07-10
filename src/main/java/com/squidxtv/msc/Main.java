1091720ed6812a5d8e6d3fea82bb921e0a7014b1
SquidXTV <squidxtv@gmail.com>
2022-07-10 17:44:28 +0200

    added static Paths

--- src/main/java/com/squidxtv/msc/Main.java
+++ src/main/java/com/squidxtv/msc/Main.java
@@ -1,6 +1,8 @@
 package com.squidxtv.msc;
 
-import java.util.concurrent.ExecutorService;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -8,18 +10,28 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     private static ScheduledExecutorService scheduledService;
+    private static Path saves;
+    private static Path backup;
+    private static Path tools;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         scheduledService = Executors.newSingleThreadScheduledExecutor();
+        saves = Files.createDirectories(Path.of(System.getenv("APPDATA"), "Minecraft Server Creator", "saves"));
+        backup = Files.createDirectories(Path.of(System.getenv("APPDATA"), "Minecraft Server Creator", "backup"));
+        tools = Files.createDirectories(Path.of(System.getenv("APPDATA")).resolve("Minecraft Server Creator").resolve("Tools"));
+//        DownloadManager.downloadBuildTools();
         App.launch(App.class, args);
     }
 
     public static ScheduledExecutorService getScheduledService() {
         return scheduledService;
     }
+    public static Path getSaves() {return saves;}
+    public static Path getBackup() {return backup;}
+    public static Path getTools() {return tools;}
 
     public static void shutdownApplication() {
-        ScheduledExecutorService ses = Main.getScheduledService();
+        ScheduledExecutorService ses = getScheduledService();
         ses.shutdown();
         try {
             if (!ses.awaitTermination(800, TimeUnit.MILLISECONDS)) {
