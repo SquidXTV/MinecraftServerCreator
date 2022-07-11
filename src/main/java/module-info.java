c23b78a849223a21a75faf226afd303c975a38c4
SquidXTV <squidxtv@gmail.com>
2022-07-11 10:53:45 +0200

    added org.json

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.squidxtv.msc {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
     opens com.squidxtv.msc to javafx.fxml;
     opens com.squidxtv.msc.util to javafx.fxml;
