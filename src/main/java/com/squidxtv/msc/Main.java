34ee6b5b81488c2a98b605ed4c5dae799aebe63a
SquidXTV <squidxtv@gmail.com>
2022-07-11 10:52:22 +0200

    added new ExecutorService + shutdown

--- src/main/java/com/squidxtv/msc/Main.java
+++ src/main/java/com/squidxtv/msc/Main.java
@@ -3,6 +3,7 @@ package com.squidxtv.msc;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -10,12 +11,15 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
     private static ScheduledExecutorService scheduledService;
+    private static ExecutorService service;
     private static Path saves;
     private static Path backup;
     private static Path tools;
 
     public static void main(String[] args) throws IOException {
         scheduledService = Executors.newSingleThreadScheduledExecutor();
+        service = Executors.newSingleThreadExecutor();
+
         saves = Files.createDirectories(Path.of(System.getenv("APPDATA"), "Minecraft Server Creator", "saves"));
         backup = Files.createDirectories(Path.of(System.getenv("APPDATA"), "Minecraft Server Creator", "backup"));
         tools = Files.createDirectories(Path.of(System.getenv("APPDATA")).resolve("Minecraft Server Creator").resolve("Tools"));
@@ -26,19 +30,25 @@ public class Main {
     public static ScheduledExecutorService getScheduledService() {
         return scheduledService;
     }
+    public static ExecutorService getService() {return service;}
     public static Path getSaves() {return saves;}
     public static Path getBackup() {return backup;}
     public static Path getTools() {return tools;}
 
     public static void shutdownApplication() {
-        ScheduledExecutorService ses = getScheduledService();
-        ses.shutdown();
+        shutdownService(getScheduledService());
+        shutdownService(getService());
+        // shutdown Process
+    }
+
+    private static void shutdownService(ExecutorService es) {
+        es.shutdown();
         try {
-            if (!ses.awaitTermination(800, TimeUnit.MILLISECONDS)) {
-                ses.shutdownNow();
+            if(!es.awaitTermination(800, TimeUnit.MILLISECONDS)) {
+                es.shutdownNow();
             }
         } catch (InterruptedException e) {
-            ses.shutdownNow();
+            es.shutdownNow();
         }
     }
 }
