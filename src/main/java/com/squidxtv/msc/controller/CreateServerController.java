f85c37ab257de19d7ff157a5c05213a15d7be508
SquidXTV <squidxtv@gmail.com>
2022-07-14 18:01:39 +0200

    added functionality and removed old class

--- src/main/java/com/squidxtv/msc/controller/CreateServerController.java
+++ src/main/java/com/squidxtv/msc/controller/CreateServerController.java
@@ -1,129 +1,26 @@
 package com.squidxtv.msc.controller;
 
 import com.squidxtv.msc.App;
+import com.squidxtv.msc.Main;
+import com.squidxtv.msc.server.spigot.SpigotVersion;
 import com.squidxtv.msc.util.Controller;
 import com.squidxtv.msc.util.Scenes;
+import com.squidxtv.msc.util.Util;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BackgroundPosition;
 import javafx.stage.DirectoryChooser;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.Future;
-
-//public class CreateServerController {
-//
-//    private DirectoryChooser directoryChooser;
-//    private File directory;
-//    private String path;
-//    private Future<?> currentFuture;
-//
-//    @FXML
-//    private Button backButton;
-//    @FXML
-//    private Button selectFolderButton;
-//    @FXML
-//    private TextField nameTextField;
-//    @FXML
-//    private Label folderLabel;
-//    @FXML
-//    private ComboBox<String> versions;
-//
-//    @FXML
-//    private Label errorMessage;
-//
-//    @FXML
-//    private void initialize() {
-//        App.setButtonBackground("arrow_left.png", backButton, 48, 48, BackgroundPosition.CENTER);
-//        directoryChooser = new DirectoryChooser();
-//        versions.setVisibleRowCount(5);
-//        for(SpigotVersion sv : SpigotVersion.values()) {
-//            versions.getItems().add(sv.name().replace("S", "Spigot - ").replace("_", "."));
-//        }
-//    }
-//
-//    @FXML
-//    private void createServer() {
-//        if(!validate()) {
-//            errorMessage.setVisible(true);
-//            currentFuture = Main.getScheduledService().schedule(() -> Platform.runLater(() -> errorMessage.setVisible(false)), 5, TimeUnit.SECONDS);
-//            return;
-//        }
-//        errorMessage.setVisible(false);
-//        if(currentFuture != null) currentFuture.cancel(true);
-//
-//        String name = nameTextField.getText();
-//        SpigotVersion version = SpigotVersion.fromVersion(versions.getValue());
-//        Path save = Path.of(Main.getSaves().toUri()).resolve(name);
-//
-//        Main.getScheduledService().schedule(() -> {
-//            try {
-//                Files.createDirectories(save);
-//                Path spigotFile = Path.of(Main.getBackup().toUri()).resolve(name.replace("Spigot - ", "spigot-") + ".jar");
-//                if(!Files.exists(spigotFile)) {
-//                    // download with buildtools into backup folder
-//                    ProcessBuilder builder = new ProcessBuilder("java", "-jar", "BuildTools.jar", "--rev", version.getBuild(), "--output-dir", Main.getBackup().toString());
-//                    builder.directory(new File(Main.getTools().toUri()));
-//                    Process p = builder.start();
-//                    p.waitFor();
-//                }
-//                Files.copy(spigotFile, save);
-//                // generate and run start.bat
-//                // accept eula
-//                // run start.bat
-//            } catch (IOException | InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }, 100, TimeUnit.MILLISECONDS);
-//    }
-//
-//    @FXML
-//    private void selectFolder() {
-//        directory = directoryChooser.showDialog(selectFolderButton.getScene().getWindow());
-//        if(directory == null) return;
-//        path = directory.getAbsolutePath().replace("\\", "/");
-//        folderLabel.setText(path);
-//    }
-//
-//    @FXML
-//    private void backButtonAction() {
-//        reset();
-//        SceneManager.getInstance().activate(Scenes.MENU);
-//    }
-//
-//    private void reset() {
-//        nameTextField.setText("");
-//        versions.setValue(null);
-//        path = "";
-//        directory = null;
-//        folderLabel.setText("");
-//        errorMessage.setVisible(false);
-//    }
-//
-//    private boolean validate() {
-//        String name = nameTextField.getText();
-//
-//        if(currentFuture != null) currentFuture.cancel(true);
-//        if(directory == null) {
-//            errorMessage.setText("No Directory selected!");
-//            return false;
-//        }
-//        if(versions.getValue() == null) {
-//            errorMessage.setText("No Version selected!");
-//            return false;
-//        }
-//        if(!Pattern.matches("^[\\w\\s.-]{1,244}$", name)) {
-//            errorMessage.setText("Name of Server is invalid, allowed characters: A-Za-z0-9._- and whitespaces!");
-//            return false;
-//        }
-//        if(Files.exists(Path.of(Main.getSaves().toUri()).resolve(name))) {
-//            errorMessage.setText("Server with that name already exists!");
-//        }
-//        return true;
-//    }
-//}
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 public class CreateServerController implements Controller<String> {
 
@@ -139,18 +36,74 @@ public class CreateServerController implements Controller<String> {
     private Label path;
     @FXML
     private Button back;
-
     @FXML
     private Label error;
+
+    @FXML
+    private void createAction() {
+        if(!validate()) {
+            error.setVisible(true);
+            future = Main.getScheduledService().schedule(() -> Platform.runLater(() -> error.setVisible(false)), 5, TimeUnit.SECONDS);
+            return;
+        }
+        error.setVisible(false);
+        if(future != null) future.cancel(true);
+
+        String n = name.getText();
+        System.out.println("Debug: " + n);
+        System.out.println("Debug: " + versions.getValue());
+        App.getManager().switchScene(Scenes.SETUP, n + "|" + versions.getValue());
+    }
 
     @FXML
     private void backAction() {
         App.getManager().switchScene(Scenes.MENU, "");
     }
 
-    @Override
-    public void init(String s) {}
+    @FXML
+    private void folderAction() {
+        dir = dirChooser.showDialog(error.getScene().getWindow());
+        if(dir == null) return;
+        path.setText(dir.getAbsolutePath().replace("\\", "/"));
+    }
 
     @Override
-    public void reset() {}
+    public void init(String s) {
+        Util.setButtonBackground("arrow_left.png", back, 48, 48, BackgroundPosition.CENTER);
+        versions.setVisibleRowCount(5);
+        for(SpigotVersion sv : SpigotVersion.values()) {
+            versions.getItems().add(sv.name().replace("S", "Spigot - ").replace("_", "."));
+        }
+    }
+
+    @Override
+    public void reset() {
+        dir = null;
+        name.setText("");
+        path.setText("");
+        versions.setValue(null);
+        if(future!=null) future.cancel(true);
+        future = null;
+        error.setVisible(false);
+    }
+
+    private boolean validate() {
+        String n = name.getText();
+        if(dir == null) {
+            error.setText("No Directory selected!");
+            return false;
+        }
+        if(versions.getValue() == null) {
+            error.setText("No Version selected!");
+            return false;
+        }
+        if(!Pattern.matches("^[\\w\\s.-]{1,244}$", n)) {
+            error.setText("Name of Server is invalid, allowed characters: A-Za-z0-9._- and whitespaces!");
+            return false;
+        }
+        if(Files.exists(Path.of(Main.getSaves().toUri()).resolve(n))) {
+            error.setText("Server with that name already exists!");
+        }
+        return true;
+    }
 }
